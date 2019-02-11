81d5816f8fe99f04e160d0b3b26c6f3ab4f91ac1
sushmitha <sushmitha51094@gmail.com>
2019-02-11 18:39:01 -0500

    Initial Commit

---
+++ Functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Functions
+//
+//  Created by MacStudent on 2019-02-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

82ae1600b1e25d76508ece0fa6c346faf8441de1
sushmitha <sushmitha51094@gmail.com>
2019-02-11 19:06:17 -0500

    functions

--- Functions/main.swift
+++ Functions/main.swift
@@ -10,3 +10,66 @@ import Foundation
 
 print("Hello, World!")
 
+func hello()
+{
+    print("hello")
+    
+}
+
+hello()
+
+func sum(a:Int,b:Int)
+{
+    let c = a+b;
+    print("sum:\(c)")
+}
+
+sum(a:10,b:40)
+
+func sum(a:Float,b:Float)
+{
+    let c = a+b
+    print("sum-F:\(c)")
+}
+
+sum(a:10.5,b:20.0)
+
+hello()
+
+func sub(a:Int,b:Int)
+{
+    let c = a-b;
+    print("sub-I:\(c)")
+}
+sub(a:10,b:5)
+
+func greet(name:String) -> String
+{
+    let s = "welcome,\(name)"
+    return s
+}
+let s = greet(name: "sushmitha")
+print(s)
+
+func exchange(a:Int,b:Int) -> (Int,Int)
+
+{
+    return (b,a)
+}
+
+var x = 100
+var y = 300
+print(x,y)
+(x,y) = exchange(a:x,b:y)
+
+print(x,y)
+
+func si(amount:Float,rate:Float,noOfYear:Float) -> Float
+
+{
+    return(amount * rate * noOfYear) / 100.0
+}
+
+print(si(amount:1000,rate:2.5,noOfYear:10))
+
+
